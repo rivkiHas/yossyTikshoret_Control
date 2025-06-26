cb72d4d7c6fd080e0615599e2a9ec324e04b916c
Rivka Hass <rivki73694@gmail.com>
2025-06-26 14:08:46 +0300

    commit

--- components/desktop/step2.jsx
+++ components/desktop/step2.jsx
@@ -14,184 +14,176 @@ import Carusel from "../carusel";
 
 
 export default function StepTwo({ brunch: propBrunch }) {
-  const dispatch = useDispatch();
-  const brunches = useSelector((state) => state.brunch.brunches);
-  const typeMarketer = useSelector((state) => state.form.pertip.typeMarketer)
-  const activeStep = useSelector((state) => state.stepper.activeStep)
-  const activeBrunch = useSelector((state) => state.brunch.activeBrunch);
+    const dispatch = useDispatch();
+    const brunches = useSelector((state) => state.brunch.brunches);
+    const typeMarketer = useSelector((state) => state.form.pertip.typeMarketer)
+    const activeStep = useSelector((state) => state.stepper.activeStep)
+    const activeBrunch = useSelector((state) => state.brunch.activeBrunch);
 
-  const brunch = propBrunch || brunches.find(b => b.id === activeBrunch);
+    const brunch = propBrunch || brunches.find(b => b.id === activeBrunch);
 
-  useEffect(() => {
-    if (propBrunch && propBrunch.id && propBrunch.id !== activeBrunch) {
-      console.log("step2", propBrunch.id);
+    useEffect(() => {
+        if (propBrunch && propBrunch.id && propBrunch.id !== activeBrunch) {
+            dispatch(setActiveBrunch(propBrunch.id));
+        }
+    }, [propBrunch, activeBrunch, dispatch]);
 
-      dispatch(setActiveBrunch(propBrunch.id));
+    const [showDialog, setShowDialog] = useState(false);
+    const [newBranchName, setNewBranchName] = useState('');
 
-    }
-  }, [propBrunch, activeBrunch, dispatch]);
-
-  const [showDialog, setShowDialog] = useState(false);
-  const [newBranchName, setNewBranchName] = useState('');
-
-  const nextStepInRedux = () => {
-    if (brunches.length === 1) {
-      dispatch(setActiveStep(activeStep + 1));
-    } else {
-      const currentBrunchIndex = brunches.findIndex(b => b.id === activeBrunch);
-      if (currentBrunchIndex < brunches.length - 1) {
-        const nextBrunch = brunches[currentBrunchIndex + 1];
-        dispatch(setActiveBrunch(nextBrunch.id));
-        dispatch(setActiveStep(2 + currentBrunchIndex + 1));
-      } else {
-        dispatch(setActiveStep(2 + brunches.length));
-      }
-    }
-  };
-
-  const previousStepInRedux = () => {
-    if (brunches.length === 1) {
-      dispatch(setActiveStep(activeStep - 1));
-    } else {
-      const currentBrunchIndex = brunches.findIndex(b => b.id === activeBrunch);
-      if (currentBrunchIndex > 0) {
-        const prevBrunch = brunches[currentBrunchIndex - 1];
-        dispatch(setActiveBrunch(prevBrunch.id));
-        dispatch(setActiveStep(2 + currentBrunchIndex - 1));
-      } else {
-        dispatch(setActiveStep(0));
-      }
-    }
-  };
-
-  const handleAddBranchClick = () => {
-    setShowDialog(true);
-  };
-
-  const handleConfirmAddBranch = () => {
-    const newId = Math.max(...brunches.map(b => b.id)) + 1;
-    const newBrunch = {
-      id: newId,
-      name: newBranchName || `${brunches.length + 1}`,
-      address: "",
-      location: {
-        lat: 32.0853,
-        lng: 34.7818
-      },
-      hoursOpen: [
-        { morning: { open: "", close: "" }, evening: { open: "", close: "" } },
-        { morning: { open: "", close: "" }, evening: { open: "", close: "" } },
-        { morning: { open: "", close: "" }, evening: { open: "", close: "" } },
-        { morning: { open: "", close: "" }, evening: { open: "", close: "" } },
-        { morning: { open: "", close: "" }, evening: { open: "", close: "" } }
-      ],
-      weekday: { morning: { open: "", close: "" }, evening: { open: "", close: "" } }
+    const nextStepInRedux = () => {
+        if (brunches.length === 1) {
+            dispatch(setActiveStep(activeStep + 1));
+        } else {
+            const currentBrunchIndex = brunches.findIndex(b => b.id === activeBrunch);
+            if (currentBrunchIndex < brunches.length - 1) {
+                const nextBrunch = brunches[currentBrunchIndex + 1];
+                dispatch(setActiveBrunch(nextBrunch.id));
+                dispatch(setActiveStep(2 + currentBrunchIndex + 1));
+            } else {
+                dispatch(setActiveStep(2 + brunches.length));
+            }
+        }
     };
-    dispatch(addBrunch(newBrunch));
-    dispatch(setActiveBrunch(newId));
 
-    const newBrunchIndex = brunches.length;
-    dispatch(setActiveStep(2 + newBrunchIndex));
+    const previousStepInRedux = () => {
+        if (brunches.length === 1) {
+            dispatch(setActiveStep(activeStep - 1));
+        } else {
+            const currentBrunchIndex = brunches.findIndex(b => b.id === activeBrunch);
+            if (currentBrunchIndex > 0) {
+                const prevBrunch = brunches[currentBrunchIndex - 1];
+                dispatch(setActiveBrunch(prevBrunch.id));
+                dispatch(setActiveStep(2 + currentBrunchIndex - 1));
+            } else {
+                dispatch(setActiveStep(0));
+            }
+        }
+    };
 
-    setShowDialog(false);
-    setNewBranchName('');
-  };
+    const handleAddBranchClick = () => {
+        setShowDialog(true);
+    };
 
-  const handleDeleteConfirmation = (brunch) => {
-    if (brunches.length > 1) {
-      const brunchIndex = brunches.findIndex(b => b.id === brunch.id);
-      dispatch(removeBrunch(brunch.id));
+    const handleConfirmAddBranch = () => {
+        const newId = Math.max(...brunches.map(b => b.id), 0) + 1;
+        const newBrunch = {
+            id: newId,
+            name: newBranchName || `${brunches.length + 1}`,
+            address: "",
+            location: {
+                lat: 32.0853,
+                lng: 34.7818
+            },
+            hoursOpen: [
+                { morning: { open: "", close: "" }, evening: { open: "", close: "" } },
+                { morning: { open: "", close: "" }, evening: { open: "", close: "" } },
+                { morning: { open: "", close: "" }, evening: { open: "", close: "" } },
+                { morning: { open: "", close: "" }, evening: { open: "", close: "" } },
+                { morning: { open: "", close: "" }, evening: { open: "", close: "" } }
+            ],
+            weekday: { morning: { open: "", close: "" }, evening: { open: "", close: "" } }
+        };
+        dispatch(addBrunch(newBrunch));
+        dispatch(setActiveBrunch(newId));
+        const newBrunchIndex = brunches.length;
+        dispatch(setActiveStep(2 + newBrunchIndex));
+        setShowDialog(false);
+        setNewBranchName('');
+    };
 
-      if (brunchIndex > 0) {
-        const prevBrunch = brunches[brunchIndex - 1];
-        dispatch(setActiveBrunch(prevBrunch.id));
-        dispatch(setActiveStep(2 + brunchIndex - 1));
-      } else if (brunches.length > 1) {
-        const nextBrunch = brunches[1];
-        dispatch(setActiveBrunch(nextBrunch.id));
-        dispatch(setActiveStep(2));
-      } else {
-        dispatch(setActiveStep(1));
-      }
-    } else {
-      console.log("לא ניתן למחוק, חייב להיות לפחות סניף אחד");
+    const handleDeleteConfirmation = (brunch) => {
+        if (brunches.length > 1) {
+            const brunchIndex = brunches.findIndex(b => b.id === brunch.id);
+            dispatch(removeBrunch(brunch.id));
+
+            if (brunchIndex > 0) {
+                const prevBrunch = brunches[brunchIndex - 1];
+                dispatch(setActiveBrunch(prevBrunch.id));
+                dispatch(setActiveStep(2 + brunchIndex - 1));
+            } else if (brunches.length > 1) {
+                const nextBrunch = brunches[1];
+                dispatch(setActiveBrunch(nextBrunch.id));
+                dispatch(setActiveStep(2));
+            } else {
+                dispatch(setActiveStep(1));
+            }
+        } else {
+            console.log("לא ניתן למחוק, חייב להיות לפחות סניף אחד");
+        }
+    };
+
+    if (!brunch) {
+        return null;
     }
-  };
 
-  if (!brunch) {
-    return null;
-  }
+    return (
+        <div className="flex justify-center ">
+            <div className="flex flex-col items-end w-full max-w-[1440px] px-[20px] py-[30px]">
+                <div className="flex flex-col lg:flex-row w-full h-full gap-[24px] lg:gap-[36px]">
+                    <div className="flex flex-col lg:flex-row w-full h-full gap-6">
+                        <div className="flex flex-col lg:w-1/2 w-full h-full">
+                            <AddressSearchMap typeMarketer={typeMarketer} />
+                        </div>
 
-  return (
-    <div className="flex justify-center ">
-      <div className="flex flex-col items-end w-full max-w-[1440px] px-[20px]  py-[30px]">
-        <div className="flex flex-col lg:flex-row w-full h-full gap-[24px] lg:gap-[36px]">
-          <div className="flex flex-col lg:flex-row w-full h-full gap-6">
-            <div className="flex flex-col lg:w-1/2 w-full h-full">
-              <AddressSearchMap typeMarketer={typeMarketer} />
-            </div>
+                        <div className="flex flex-col lg:w-1/2 w-full h-full lg:h-auto p-4 bg-white rounded-[40px]">
+                            <HoursOpen typeMarketer={typeMarketer} />
+                            <Button onClick={handleAddBranchClick}
+                                className="lg:hidden cursor-pointer bg-black border hover:bg-white hover:text-black hover:border-black text-white p-5 gap-2 rounded-full"
+                            >
+                                <PlusCircleIcon className="w-5 h-5" />
+                                הוספת סניף נוסף
+                            </Button>
+                        </div>
+                    </div>
+                </div>
 
-            <div className="flex flex-col lg:w-1/2 w-full h-full lg:h-auto p-4 bg-white rounded-[40px]">
-              <HoursOpen typeMarketer={typeMarketer} />
-              <Button onClick={handleAddBranchClick}
-                className="lg:hidden cursor-pointer bg-black border hover:bg-white hover:text-black hover:border-black text-white p-5 gap-2 rounded-full"
-              >
-                <PlusCircleIcon />
-                הוספת סניף נוסף
-              </Button>
-            </div>
-          </div>
-        </div>
+                <div className="flex w-full items-center justify-between mt-8">
+                    {typeMarketer === "חנות" ? (
+                        <div className='hidden lg:flex flex-row gap-4'>
+                            <Button onClick={handleAddBranchClick}
+                                className="cursor-pointer bg-black border hover:bg-white hover:text-black hover:border-black text-white p-5 gap-2 rounded-full"
+                            >
+                                <PlusCircleIcon className="w-5 h-5" />
+                                הוספת סניף נוסף
+                            </Button>
 
-             <div className={`flex flex-row w-full mt-6 items-start ${typeMarketer === "חנות" ? "justify-between" : "justify-end"}`}>
-        {typeMarketer === "חנות" ? (
-          <div>
-            <div className='hidden lg:flex flex-row gap-4'>
-              <Button onClick={handleAddBranchClick}
-                className="cursor-pointer bg-black border hover:bg-white hover:text-black hover:border-black text-white p-5 gap-2 rounded-full"
-              >
-                <PlusCircleIcon />
-                הוספת סניף נוסף
-              </Button>
+                            {brunches.length > 1 && brunch && (
+                                <IconButton
+                                    headerText="מחיקה"
+                                    onConfirm={() => handleDeleteConfirmation(brunch)}
+                                    contactId={brunch.id}
+                                    text="האם ברצונך למחוק את הסניף הזה?"
+                                />
+                            )}
+                        </div>
+                    ) : (
+                        <Carusel activeBrunch={activeBrunch} />
+                    )}
 
-              {brunches.length > 1 && brunch && (
-                <IconButton
-                  headerText="מחיקה"
-                  onConfirm={() => handleDeleteConfirmation(brunch)}
-                  contactId={brunch.id}
-                  text="האם ברצונך למחוק את הסניף הזה?"
+                    <div className="hidden lg:flex gap-4">
+                        <Button onClick={previousStepInRedux}
+                            className="cursor-pointer flex items-center bg-white text-black border border-[#F8BD00]  p-5 gap-3 rounded-full hover:bg-white hover:text-black hover:border-black"
+                        >
+                            <ArrowLongRightIcon className="w-5 h-5" />
+                            שלב קודם
+                        </Button>
+                        <Button onClick={nextStepInRedux}
+                            className="cursor-pointer flex items-center gap-2 bg-yellow-400 text-black p-5 rounded-full border border-transparent hover:bg-white hover:text-black hover:border-[#F8BD03]">
+                            שלב הבא
+                            <ArrowLongLeftIcon className="w-5 h-5" />
+                        </Button>
+                    </div>
+                </div>
+
+                <AlertDialogEdit
+                    open={showDialog}
+                    value={newBranchName}
+                    onChange={setNewBranchName}
+                    onConfirm={handleConfirmAddBranch}
+                    onCancel={() => setShowDialog(false)}
                 />
-              )}
             </div>
-          </div>
-        ) : (
-          <Carusel activeBrunch={activeBrunch} />
-        )}
-
-        <div className="hidden lg:flex gap-4">
-          <Button onClick={previousStepInRedux}
-            className="cursor-pointer flex items-center bg-white text-black border border-[#F8BD00]  p-5 gap-3 rounded-full hover:bg-white hover:text-black hover:border-black"
-          >
-            <ArrowLongRightIcon />
-            שלב קודם
-          </Button>
-          <Button onClick={nextStepInRedux}
-            className="cursor-pointer flex items-center gap-2 bg-yellow-400 text-black p-5 rounded-full border border-transparent hover:bg-white hover:text-black hover:border-[#F8BD03]">
-            שלב הבא
-            <ArrowLongLeftIcon />
-          </Button>
         </div>
-      </div>
-
-      <AlertDialogEdit
-        open={showDialog}
-        value={newBranchName}
-        onChange={setNewBranchName}
-        onConfirm={handleConfirmAddBranch}
-        onCancel={() => setShowDialog(false)}
-      />
-    </div>
-  </div>
-
-  );
+    );
 }
--- components/desktop/tabs2.jsx
+++ components/desktop/tabs2.jsx
@@ -1,37 +1,150 @@
-import React from 'react'
-import StepOne from './step1'
-import StepTwo from './step2'
-import StepThree from './step3'
+import React, { useState, useEffect, useRef } from 'react';
+import StepOne from './step1';
+import StepTwo from './step2';
+import StepThree from './step3';
+import { BuildingStorefrontIcon, MapPinIcon, UserCircleIcon } from '@heroicons/react/24/outline';
+
+const stepsData = [
+  {
+    id: 1,
+    title: 'שלב ראשון',
+    subtitle: 'פרטים על העסק',
+    icon: BuildingStorefrontIcon,
+    targetId: 'step-one',
+    statusIcon: '✓',
+  },
+  {
+    id: 2,
+    title: 'שלב שני',
+    subtitle: 'כתובת ושעות העסק',
+    icon: MapPinIcon,
+    targetId: 'step-two',
+    statusIcon: '!',
+  },
+  {
+    id: 3,
+    title: 'שלב שלישי',
+    subtitle: 'פרטי איש קשר',
+    icon: UserCircleIcon,
+    targetId: 'step-three',
+    statusIcon: '3',
+  },
+];
 
 const Tabs2 = () => {
+  const [activeStep, setActiveStep] = useState(1);
+  const [isCompleted, setIsCompleted] = useState(false);
+  const sectionRefs = useRef([]);
+
+  useEffect(() => {
+    const observerOptions = {
+      root: null,
+      rootMargin: '0px 0px -60% 0px',
+      threshold: 0,
+    };
+
+    const observerCallback = (entries) => {
+      entries.forEach(entry => {
+        if (entry.isIntersecting && !isCompleted) {
+          const stepId = parseInt(entry.target.dataset.stepId, 10);
+          setActiveStep(stepId);
+        }
+      });
+    };
+
+    const observer = new IntersectionObserver(observerCallback, observerOptions);
+    const refs = sectionRefs.current;
+    refs.forEach(ref => {
+      if (ref) observer.observe(ref);
+    });
+
+    return () => {
+      refs.forEach(ref => {
+        if (ref) observer.unobserve(ref);
+      });
+    };
+  }, [isCompleted]);
+
+  const handleStepClick = (step) => {
+    setActiveStep(step.id);
+    if (isCompleted) {
+      setIsCompleted(false);
+    }
+    document.getElementById(step.targetId)?.scrollIntoView({ behavior: 'smooth' });
+  };
+
+  const submitForm = () => {
+    alert("הטופס נשלח!");
+  };
+
+  const completeProcess = () => {
+    setIsCompleted(true);
+  };
+
   return (
     <div>
-       <div id='step-one'> <StepOne /></div>
-       <div id='step-two'> <StepTwo /></div>
-      <div id='step-three'>  <StepThree /></div>
-      <div className="fixed top-[80vh] right-4 z-50 flex items-center bg-yellow-400 rounded-full px-4 py-2 shadow-md w-fit">
- 
-   <a href="#step-three" className="bg-black text-white rounded-full flex items-center px-3 py-1 mx-1">
-    <img src="/images/homeIcon.png" alt="אייקון 3" className="w-5 h-5 ml-2" />
-    <div className="text-right">
-      <div className="text-sm font-bold">שלב ראשון</div>
-      <div className="text-xs">פרטים על העסק</div>
+      <div id="step-one" ref={el => sectionRefs.current[0] = el} data-step-id="1"><StepOne /></div>
+      <div id="step-two" ref={el => sectionRefs.current[1] = el} data-step-id="2"><StepTwo /></div>
+      <div id="step-three" ref={el => sectionRefs.current[2] = el} data-step-id="3">
+        <StepThree onComplete={completeProcess} />
+      </div>
+
+      <div className="fixed bottom-4 left-0 right-0 z-50 flex justify-center px-4">
+        {isCompleted ? (
+          <div className="flex items-center justify-between bg-yellow-400 rounded-full mx-auto w-[345px] py-2 px-[22px] shadow-lg">
+            <div className="flex items-center">
+              {stepsData.map(step => {
+                const Icon = step.icon;
+                return (
+                  <button key={step.id} onClick={() => handleStepClick(step)} className="relative bg-white rounded-full w-[43px] h-[43px] flex items-center justify-center mx-1 cursor-pointer">
+                    <Icon className="w-5 h-5 text-black" />
+                    {step.statusIcon && (
+                      <span className="absolute -top-1 -right-1 bg-black text-white text-xs rounded-full w-5 h-5 flex items-center justify-center border-2 border-yellow-400 font-bold">
+                        {step.statusIcon}
+                      </span>
+                    )}
+                  </button>
+                );
+              })}
+            </div>
+            <button onClick={submitForm} className="bg-white text-black rounded-full flex items-center px-6 py-2 ml-1 font-bold">
+              <span className="text-lg mr-2">→</span>
+              <span>שלח</span>
+            </button>
+          </div>
+        ) : (
+          <div className="flex items-center justify-evenly bg-yellow-400 rounded-full mx-auto w-[345px] py-2 px-[22px] shadow-lg">
+            {stepsData.map(step => {
+              const Icon = step.icon;
+              return (
+                <button
+                  key={step.id}
+                  onClick={() => handleStepClick(step)}
+                  className="flex items-center justify-center transition-all duration-300 ease-in-out"
+                >
+                  {activeStep === step.id ? (
+                    <div className="flex items-center gap-2 px-2">
+                      <div className="bg-black rounded-full w-[53px] h-[53px] flex items-center justify-center flex-shrink-0">
+                        <Icon className="w-7 h-7 text-white" /> 
+                      </div>
+                      <div className="text-left text-black">
+                        <div className="text-sm font-bold">{step.title}</div>
+                        <div className="text-xs">{step.subtitle}</div>
+                      </div>
+                    </div>
+                  ) : (
+                    <div className="bg-white rounded-full w-[43px] h-[43px] flex items-center justify-center flex-shrink-0">
+                      <Icon className="w-6 h-6 text-black" />
+                    </div>
+                  )}
+                </button>
+              );
+            })}
+          </div>
+        )}
+      </div>
     </div>
-  </a>
-  <a href="#step-one" className="bg-white rounded-full w-10 h-10 flex items-center justify-center mx-1">
-    <img src="/images/homeIcon.png" alt="אייקון 1" className="w-5 h-5" />
-  </a>
+  );
+};
 
-  {/* כפתור שני - עיגול לבן עם אייקון */}
-  <a href="#step-two" className="bg-white rounded-full w-10 h-10 flex items-center justify-center mx-1">
-    <img src="/images/homeIcon.png" alt="אייקון 2" className="w-5 h-5" />
-  </a>
- 
-</div>
- 
-
-    </div>
-  )
-}
-
-export default Tabs2
+export default Tabs2;
